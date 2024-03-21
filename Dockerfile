8a6659188037aaa731ae7ded08a918ff8d486440
garage <garagekandau@gmail.com>
2024-03-21 13:32:48 +0300

    fixes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM arm64v8/eclipse-temurin:17
+EXPOSE 8080
 VOLUME /tmp
-ARG JAR_FILE
-COPY ${JAR_FILE} app.jar
+COPY target/*.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
---
+++ src/main/java/com/mean/cicd/Main.java
@@ -0,0 +1,10 @@
+package com.mean.cicd;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i=0; i <= 5;){
+            i++;
+            System.out.println(i);
+        }
+    }
+}
